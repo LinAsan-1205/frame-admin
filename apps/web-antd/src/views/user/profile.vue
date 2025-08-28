5adae0249284623d7911e81467ab1e746ef20232
linasan <1603889221@qq.com>
2025-08-28 11:53:03 +0800

    feat: 更新用户个人资料页面，添加侧边菜单和基础设置选项，优化用户界面布局

--- apps/web-antd/src/views/user/profile.vue
+++ apps/web-antd/src/views/user/profile.vue
@@ -1,4 +1,35 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import { Page } from '@vben/common-ui';
+
+import { Menu } from 'ant-design-vue';
+
+const items = [
+  {
+    key: 'foundation',
+    label: '基础设置',
+  },
+  {
+    key: 'safe',
+    label: '安全设置',
+  },
+];
+const activeKey = ref(['foundation']);
+</script>
 <template>
-  <div>userProfile</div>
+  <Page>
+    <div class="card-box p-5">
+      <div class="flex gap-7">
+        <div class="flex min-w-[200px] flex-shrink-0">
+          <Menu
+            :items="items"
+            mode="inline"
+            v-model:selected-keys="activeKey"
+          />
+        </div>
+        <div class="flex flex-1">333</div>
+      </div>
+    </div>
+  </Page>
 </template>
