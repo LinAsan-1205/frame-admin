1959f5b851eba13183bd8993cbcbcc58e2b7092f
sansan <1603889221@qq.com>
2025-09-13 23:29:28 +0800

    feat: 优化用户头像处理逻辑，使用默认头像替代空头像，提升用户界面体验

--- apps/web-antd/src/components/model/system/user/user-block.vue
+++ apps/web-antd/src/components/model/system/user/user-block.vue
@@ -1,26 +1,32 @@
 <script setup lang="ts">
 import type { User } from '#/api/system/user';
 
+import { computed } from 'vue';
+
 import { EllipsisText } from '@vben/common-ui';
 import { IconifyIcon } from '@vben/icons';
 
 import { Avatar } from 'ant-design-vue';
+
+import defaultAvatar from '#/assets/images/user/avatar.png';
 
 const { origin } = defineProps<{
   origin: User.View;
 }>();
 
 const { phone, nickName, avatar } = origin;
+
+const avatarUrl = computed(() => {
+  return avatar || defaultAvatar;
+});
 </script>
 <template>
   <div class="flex w-full items-center justify-center gap-2">
     <div class="w-[40px]">
-      <Avatar :size="40" :src="avatar">
-        <template #icon>
-          <div class="flex h-full items-center justify-center">
-            <IconifyIcon icon="ant-design:user-outlined" />
-          </div>
-        </template>
+      <Avatar :size="40" :src="avatarUrl">
+        <div class="flex h-full items-center justify-center pt-[10px]">
+          <IconifyIcon icon="ant-design:user-outlined" />
+        </div>
       </Avatar>
     </div>
     <div class="flex w-[100px] flex-col justify-start">
